a733c6f2b6fbab78646eb4d8491c98b0600a3bcf
anthony81799 <58193670+anthony81799@users.noreply.github.com>
2020-04-23 22:47:13 -0400

    Update GUIController.java

--- src/main/java/controller/GUIController.java
+++ src/main/java/controller/GUIController.java
@@ -147,42 +147,44 @@ public class GUIController extends UMLController {
 		}
 		return result;
 	}
-	
-	@Override	
-	public ArrayList<String[]> printClasses() {	
-		ArrayList<String[]> result = model.printClasses();	
-		if(result != null) {	
-			// Notify observer of change	
-			notify("printClasses", model);	
-		}	
-		return result;	
-	}	
 
-	@Override	
-	public ArrayList<ArrayList<String[]>> printRelationships() {	
-		ArrayList<ArrayList<String[]>> result =model.printRelationships();	
-		if(result != null)	
-			// Notify observer of change	
-			notify("printRelationships", model);	
-		return result;	
-	}	
-
-	@Override	
-	public String[] printClasses(String className) {	
-		String[] result = model.printClasses(className);	
-		if(result != null) {	
-			// Notify observer of change	
-			notify("printClasses", model);	
-		}	
-		return result;		
-	}	
-
-	@Override	
-	public ArrayList<String[]> printRelationships(String className) {	
-		ArrayList<String[]> result = model.printRelationships(className);	
-		if(result != null) {	
-			// Notify observer of change	
-			notify("printRelationships", model);	
-		}	
+	@Override
+	public ArrayList<String[]> printClasses() {
+		ArrayList<String[]> result = model.printClasses();
+		if(result != null) {
+			// Notify observer of change
+			notify("printClasses", model);
+		}
 		return result;
+	}
+
+	@Override
+	public ArrayList<ArrayList<String[]>> printRelationships() {
+		ArrayList<ArrayList<String[]>> result = model.printRelationships();
+		if(result != null)
+			// Notify observer of change
+			notify("printRelationships", model);
+		return result;
+	}
+
+	@Override
+	public String[] printClasses(String className) {
+		String[] result = model.printClasses(className);
+		if(result != null) {
+			// Notify observer of change
+			notify("printClasses", model);
+		}
+		return result;	
+	}
+
+	@Override
+	public ArrayList<String[]> printRelationships(String className) {
+		ArrayList<String[]> result = model.printRelationships(className);
+		if(result != null) {
+			// Notify observer of change
+			notify("Relationships", model);
+		}
+		return result;
+	}
+
 }
