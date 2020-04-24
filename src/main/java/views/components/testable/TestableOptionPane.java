3ccf0ff665a92ca4091928936c1c021c3e7da20e
Ryan <haasryanj@gmail.com>
2020-04-24 13:04:18 -0400

    Add JaCoco coverage report creation to mvn execute

---
+++ src/main/java/views/components/testable/TestableOptionPane.java
@@ -0,0 +1,23 @@
+// Package name
+package views.components.testable;
+
+// System imports
+import javax.swing.JOptionPane;
+
+// Local imports
+
+/**
+ * Mock JOptionPane for testing purposes
+ * @author Ryan
+ */
+public class TestableOptionPane extends JOptionPane {
+	private String result;
+	
+	/**
+	 * Initialize instance of a testable JOptionPane
+	 * @param desiredResult - the result you want for the option pane
+	 */
+	public TestableOptionPane(String desiredResult) {
+		result = desiredResult;
+	}
+}
